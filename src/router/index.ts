0494dac60b1a6403b658aaa732da3af920838d34
kingford <32887862+skingford@users.noreply.github.com>
2021-10-27 17:00:02 +0800

    feat: 优化路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: kingford
  * @Date: 2021-10-22 09:48:04
- * @LastEditTime: 2021-10-27 11:14:49
+ * @LastEditTime: 2021-10-27 16:57:51
  */
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
@@ -9,43 +9,14 @@ import { basicRoutes } from './routes';
 
 Vue.use(VueRouter);
 
-// const routes: Array<RouteConfig> = [
-//   {
-//     path: '/',
-//     redirect: '/android',
-//   },
-//   {
-//     path: '/about',
-//     name: 'About',
-//     component: () => import('../views/About.vue'),
-//   },
-//   {
-//     path: '/android',
-//     name: 'Android',
-//     component: () => import('../views/android/index.vue'),
-//   },
-//   {
-//     path: '/lock',
-//     name: 'Lock',
-//     component: () => import('../views/android/lock.vue'),
-//   },
-//   {
-//     path: '/database',
-//     name: 'Database',
-//     component: () => import('../views/android/database.vue'),
-//   },
-//   {
-//     path: '/file',
-//     name: 'File',
-//     component: () => import('../views/android/file.vue'),
-//   },
-// ];
+const createRouter = () =>
+  new VueRouter({
+    mode: 'hash',
+    scrollBehavior: () => ({ y: 0, x: 0 }),
+    base: process.env.BASE_URL,
+    routes: basicRoutes as Array<RouteConfig>,
+  });
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  //routes,
-  routes: basicRoutes as Array<RouteConfig>,
-});
+const router = createRouter();
 
 export default router;
