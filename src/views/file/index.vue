2b19839c7571d3fbb9def079b6013964287664c7
kingford <32887862+skingford@users.noreply.github.com>
2021-10-28 14:31:59 +0800

    feat: 文件操作

--- src/views/file/index.vue
+++ src/views/file/index.vue
@@ -1,11 +1,11 @@
 <!--
  * @Author: kingford
  * @Date: 2021-10-27 17:32:56
- * @LastEditTime: 2021-10-28 11:42:40
+ * @LastEditTime: 2021-10-28 14:29:07
 -->
 <template>
   <van-cell-group>
-    <van-cell icon="setting-o" title="读文件" />
+    <van-cell icon="setting-o" title="读文件" @click="testRead" />
     <van-cell icon="setting-o" title="写文件" />
     <van-cell icon="setting-o" title="分页读取文件内容" />
     <van-cell icon="setting-o" title="获取文件家下所有文件列表" />
@@ -13,7 +13,7 @@
 </template>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { file } from '@/utils/android';
+import { file } from '@/utils/android/file';
 
 @Component({ name: 'File' })
 export default class FileComp extends Vue {
